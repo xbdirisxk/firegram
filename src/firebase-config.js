6d52da1b7c02877e97a04cbf673fd7cffc26fbc4
xbdirisxk <abdirisakjr@gmail.com>
2022-02-07 22:43:24 +0300

    initial

---
+++ src/firebase-config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDqhhIeHlSuTrk-RkmfRY7UvbhxuiES2Jo',
+    authDomain: 'firegram-6555e.firebaseapp.com',
+    projectId: 'firegram-6555e',
+    storageBucket: 'firegram-6555e.appspot.com',
+    messagingSenderId: '771617296920',
+    appId: '1:771617296920:web:b123bc54a2bfe75863696d',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
