422cb6e13161e9fe83646842b3dd1056de2094a6
xbdirisxk <abdirisakjr@gmail.com>
2022-02-09 07:30:42 +0300

    hooks

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    const types = ['image/png', 'image/jpeg'];
+
     const changeHandler = (e) => {
         let selected = e.target.files[0];
-        console.log(selected);
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+        } else {
+            setFile(null);
+            setError('please choose a jpeg or png image');
+        }
     };
     return (
         <form>
             <input type='file' onChange={changeHandler} />
+            <div className='output'>
+                {error && <div className='error'>{error}</div>}
+                {file && <div className='file'>{file.name}</div>}
+            </div>
         </form>
     );
 };
